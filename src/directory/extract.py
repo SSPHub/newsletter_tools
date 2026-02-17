282eb999865bb4341eb84e71e474aba7d6ac8bee
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-02-17 16:35:41 +0000

    refactor - directory

---
+++ src/directory/extract.py
@@ -0,0 +1,56 @@
+def get_directory_as_df():
+    """
+    Fetch back directory of SSPHUB and management as a pl dataframe
+
+    Args:
+        None
+
+    Returns:
+        A pl.DataFrame with three columns : ['Email', 'Nom', 'Nom_domaine', 'Supprimez_mon_compte']
+    """
+    # fetch all the rows of general enlisted persons
+    directory_df = fetch_grist_table_as_pl(
+        os.environ["GRIST_SSPHUB_DIRECTORY_ID"], "Contact"
+    )
+
+    # Selecting set of columns
+    cols_to_keep = ["Email", "Nom", "Nom_domaine", "Supprimez_mon_compte"]
+
+    directory_df = directory_df.select(cols_to_keep)
+
+    # Fetching data for management
+    # Selecting set of columns
+    cols_to_keep = ["Email", "Nom", "Nom_domaine"]
+
+    directory_df_mngmt = (
+        fetch_grist_table_as_pl(
+            os.environ["GRIST_SSPHUB_DIRECTORY_ID"], "Encadrement_SSMs"
+        )
+        .select(cols_to_keep)
+        .with_columns(Supprimez_mon_compte=False)
+    )
+
+    return pl.concat([directory_df, directory_df_mngmt])
+
+
+def get_emails():
+    """
+    Extract all emails that have not asked to be deteled from directory
+
+    Returns:
+        a single string with joined emails separated by ;
+
+    Example:
+        >>> get_emails()
+        '<myemail@example.com>; <myemail2@example.com>'
+    """
+    my_directory_df = get_directory_as_df()
+    my_directory_df = (
+        my_directory_df.filter(~pl.col("Supprimez_mon_compte"))  # ~ is equivalent to not
+        .unique(subset="Email")
+        .sort(["Nom_domaine", "Nom"])
+    )
+    # Turning emails from myemail@example.com to <myemail@example.com>
+    my_directory_df.with_columns("<" + pl.col("Email") + ">")
+    # Joining all emails into one string '<myemail@example.com>; <myemail2@example.com>'
+    return "; ".join(my_directory_df["Email"])
---
+++ src/directory/replies.py
@@ -0,0 +1,73 @@
+def extract_emails_from_txt(file_path="newsletter_tools/test/replies.txt"):
+    """
+    Extract all email addresses from a file that contains all the automatic replies to a newsletter / an email.
+
+    Args:
+        file_path (str): The path to the file containing email addresses.
+
+    Returns:
+        list: A list of extracted email addresses.
+    """
+    # Regular expression pattern for matching email addresses
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+
+    # Read the content of the file
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Find all email addresses in the content
+    emails = re.findall(email_pattern, content)
+
+    # Remove duplicate - Convert the list of email addresses to a set and back to list
+    emails = set(emails)
+    emails = list(emails)
+
+    return emails
+
+
+def get_ids_of_email(emails_list):
+    """
+    Return the ids of the rows of the email in the GRIST directory. Will return a list of length 0 if an email is not in the list.
+
+    Args:
+        emails_list (list) : a list of strings, containing the emails to look for in df
+
+    Returns:
+        list: A list of ids of the rows in df
+
+    Example:
+        >>> get_ids_of_email(['test1@insee.fr', 'test2@insee.fr'])
+        []
+    """
+    # Get the latest GRIST directory
+    directory_df = fetch_grist_table_as_pl(
+        os.environ["GRIST_SSPHUB_DIRECTORY_ID"], "Contact"
+    )
+    directory_df = directory_df.select(["id", "Email"])
+
+    # Filter the emails
+    res = directory_df.filter(pl.col("Email").is_in(emails_list))
+    res = pl.Series(res.select(pl.col("id"))).to_list()
+
+    return res
+
+
+def delete_email_from_contact_table(file_path):
+    """
+    Takes a txt file as input and delete the detected email from the Contacts table of Grist
+
+    Args:
+        file_path (string): path to the txt file to extract emails from
+
+    Return:
+        None
+    """
+    emails_list = extract_emails_from_txt(file_path)
+    print(str(len(emails_list)) + " emails extraits du fichier: ", emails_list)
+    emails_id = get_ids_of_email(emails_list)
+    print(str(len(emails_id)) + " emails trouvés dans la table Contact \n")
+    get_dinum_grist_login(os.environ["GRIST_SSPHUB_DIRECTORY_ID"]).delete_records(
+        "Contact", emails_id
+    )
+    print("Emails supprimés de la table Contact \n")
+
