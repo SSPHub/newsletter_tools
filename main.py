06f7b6c1c57de61e698c8c157b79bc39e7eb1122
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-02-18 10:18:02 +0000

    update main to use from CLI

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import os
 from src.generate_email import generate_email
+from src.directory.extract import get_emails
+import argparse
 
-def main(newsletter_nb: int=22, branch:str = 'main', email_object: str="[SSPHub] Infolettre de janvier"):
+def main(*args, **kwargs):
     """
-    Extract and add articles from tchap group to veille table on grist
+    Generate email based on generate_email
 
     Args:
-        how : choice between 'Test' and 'Veille' modalities
+        See generate_email doc
 
     """
-    generate_email(
-        number=newsletter_nb,
-        branch=branch,
-        email_object=f'Pour validation - {email_object}',
-        email_to=os.environ["EMAIL_VALIDATION_TO"],
-        email_bcc="",
-        email_from=None,
-        email_cc=os.environ["EMAIL_VALIDATION_CC"] + ";" + os.environ["EMAIL_SSPHUB"],
-    )
+    generate_email(*args, **kwargs)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='Generate email', description='Generate an email from the newsletter stored as a qmd file on the ssphub repo')
+    parser.add_argument("-n", "--number", default=22)
+    parser.add_argument("-b", "--branch", default="main")
+    parser.add_argument("-o", "--email_object", default="[SSPHub] Infolettre de janvier")
+    parser.add_argument("-to", "--email_to", default=os.environ['EMAIL_SSPHUB'])
+    parser.add_argument("-bcc", "--email_bcc", default=get_emails())
+    parser.add_argument("-from", "--email_from", default="")
+    parser.add_argument("-cc", "--email_cc", default="")
+    parser.add_argument("-t", "--drop_temp", default=True)
+
+    args = parser.parse_args()
+
     try:
-        # main("Test")
-        main()
+        main(args.number, args.branch, args.email_object, args.email_to, args.email_bcc, args.email_from, args.email_cc, args.drop_temp)
     except Exception as e:
         print(f"Fatal error in pipeline: {e}")
         raise
+
+
