fc49642f0118439768bc6a696fc7cc1ae2457f13
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2026-03-13 17:24:59 +0000

    with code to generate call graphs

---
+++ docs/graphs.sh
@@ -0,0 +1,33 @@
+# sudo apt-get install graphviz
+# sudo apt-get update
+
+# All
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-print | xargs code2flow --output docs/call_graph_all.png
+
+# All but test functions
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-not -path "./src/test/*" \
+-print | xargs code2flow --output docs/call_graph_all_but_test.png
+
+# Focus main
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-print | xargs code2flow --output docs/call_graph_main.png --target-function main::main --upstream-depth 10 --downstream-depth 10
+
+# Focus clearance
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-print | xargs code2flow --output docs/call_graph_clearance.png --target-function clearance::main --upstream-depth 10 --downstream-depth 10
+
+# Focus clearance
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-print | xargs code2flow --output docs/call_graph_tchap.png --target-function tchap::main --upstream-depth 10 --downstream-depth 10
+
+# Focus clearance
+find . -type f -name "*.py" \
+-not -path "./.venv/*" \
+-print | xargs code2flow --output docs/call_graph_treat_replies.png --target-function treat_replies::main --upstream-depth 10 --downstream-depth 10
